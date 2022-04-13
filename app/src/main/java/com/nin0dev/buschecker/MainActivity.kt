f486e0787696e9430216d780b7341340ff28f89e
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-13 13:11:24 -0400

    Launch setup activity

--- app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
+++ app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nin0dev.buschecker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Run setup activity, thanks!
+        val i = Intent(this, SetupActivity::class.java)
+        startActivity(i)
     }
 }
