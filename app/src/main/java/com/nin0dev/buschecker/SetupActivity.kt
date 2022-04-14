17ad588b9aea0f88deef2740403baa3edf522587
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-14 07:19:56 -0400

    Added permissions logic

--- app/src/main/java/com/nin0dev/buschecker/SetupActivity.kt
+++ app/src/main/java/com/nin0dev/buschecker/SetupActivity.kt
@@ -1,15 +1,20 @@
 package com.nin0dev.buschecker
 
 import android.app.Activity
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.content.res.Configuration
+import android.opengl.Visibility
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.color.DynamicColors
 import java.security.AccessController.getContext
 import java.util.jar.Manifest
@@ -20,7 +25,9 @@ class SetupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
         buttons()
+        components()
         startImageTint()
+        updateActionBarText("Setup")
     }
     fun startImageTint()
     {
@@ -86,23 +93,54 @@ class SetupActivity : AppCompatActivity() {
     }
     fun buttons()
     {
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            switchScreens(findViewById(R.id.start), findViewById(R.id.permissions), "Permissions")
+        }
         val notificationsButton = findViewById<Button>(R.id.grantNotificationsButton)
         notificationsButton.setOnClickListener {
             ActivityCompat.requestPermissions(this,
                 arrayOf("android.permission.POST_NOTIFICATIONS"), 1
             )
-
+            if(ContextCompat.checkSelfPermission(this, "android.permission.POST_NOTIFICATIONS") == PERMISSION_GRANTED)
+            {
+                // Permission granted
+                notificationsButton.isEnabled = false
+                notificationsButton.text = "Granted"
+            }
+            if(ContextCompat.checkSelfPermission(this, "android.permission.POST_NOTIFICATIONS") == PERMISSION_DENIED)
+            {
+                // Permission denied
+                notificationsButton.text = "Grant"
+                //return@setOnClickListener
+            }
         }
         val smsButton = findViewById<Button>(R.id.grantSMSButton)
         smsButton.setOnClickListener {
             ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.SEND_SMS), 1
+                arrayOf("android.permission.SEND_SMS"), 1
             )
+            if(ContextCompat.checkSelfPermission(this, "android.permission.SEND_SMS") == PERMISSION_GRANTED)
+            {
+                // Permission granted
+                smsButton.isEnabled = false
+                smsButton.text = "Granted"
+            }
+            if(ContextCompat.checkSelfPermission(this, "android.permission.SEND_SMS") == PERMISSION_DENIED)
+            {
+                // Permission denied
+
+                smsButton.text = "Grant"
+                //return@setOnClickListener
+            }
 
         }
     }
     fun components()
     {
-
+        if(Build.VERSION.PREVIEW_SDK_INT == 0)
+        {
+            findViewById<MaterialCardView>(R.id.notificationsCard).visibility = GONE
+        }
     }
 }
