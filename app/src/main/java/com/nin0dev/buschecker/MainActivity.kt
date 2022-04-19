c057207c01c85fe0db9aa37b3ecf903bf074d24f
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-19 11:02:32 -0400

    Added setup done/not done

--- app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
+++ app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.nin0dev.buschecker
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        DynamicColors.applyIfAvailable(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Run setup activity, thanks!
-        val i = Intent(this, SetupActivity::class.java)
-        startActivity(i)
+        val sp = getSharedPreferences("e", Context.MODE_PRIVATE)
+        if(ContextCompat.checkSelfPermission(this, "android.permission.SEND_SMS") == PackageManager.PERMISSION_DENIED)
+        {
+            val i = Intent(this, SetupActivity::class.java)
+            startActivity(i)
+        }
     }
 }
