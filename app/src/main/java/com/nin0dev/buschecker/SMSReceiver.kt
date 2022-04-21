74b3806e70f09cabeebdcbcbde658082563808aa
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-21 15:00:23 -0400

    Added phone# check

--- app/src/main/java/com/nin0dev/buschecker/SMSReceiver.kt
+++ app/src/main/java/com/nin0dev/buschecker/SMSReceiver.kt
@@ -3,16 +3,38 @@ package com.nin0dev.buschecker
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Build
 import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
 
 
 class SMSReceiver : BroadcastReceiver() {
 
     val pdu_type = "pdus"
     override fun onReceive(context: Context, intent: Intent) {
-        Log.e("SMS BusChecker", "received")
+        val bundle = intent.extras
+        val msgs: Array<SmsMessage?>
+        var strMessage = ""
+        val format = bundle!!.getString("format")
+        val pdus = bundle!![pdu_type] as Array<Any>?
+        if (pdus != null) {
+            val isVersionM = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            msgs = arrayOfNulls(pdus.size)
+            for (i in msgs.indices) {
+                msgs[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray, format)
+                val address = msgs[i]?.originatingAddress
+                val text = msgs[i]?.messageBody
+                if(address != "52786")
+                {
+                    return
+                }
+
+            }
+        }
     }
 }
