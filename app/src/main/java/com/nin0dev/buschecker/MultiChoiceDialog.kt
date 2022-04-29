1774a17d93469ffb58d864bf7041ec57379fcd6f
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-29 17:04:30 -0400

    Finished multi-choice dialog

---
+++ app/src/main/java/com/nin0dev/buschecker/MultiChoiceDialog.kt
@@ -0,0 +1,43 @@
+package com.nin0dev.buschecker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.textfield.TextInputLayout
+
+class MultiChoiceDialog : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        DynamicColors.applyIfAvailable(this)
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_multi_choice_dialog)
+        findViewById<TextView>(R.id.availableRoutesText).text = "Available routes for stop " + intent.getStringExtra("stop") + ":" + intent.getStringExtra("routes")
+        val continueB = findViewById<Button>(R.id.continueButton22)
+        val textField = findViewById<EditText>(R.id.routeNumber_multichoice_field)
+        val textFieldLayout = findViewById<TextInputLayout>(R.id.routeNumber_multichoice_layout)
+        continueB.setOnClickListener {
+            if(intent.getStringExtra("routes")?.contains(textField.text, true) == false)
+            {
+                textFieldLayout.error = "Route doesn't exist"
+                textField.setText("")
+            }
+            else
+            {
+                textFieldLayout.error = ""
+                val smsManager: SmsManager = SmsManager.getDefault()
+                smsManager.sendTextMessage("52786", null, intent.getStringExtra("stop") +  " " + textField.text, null, null)
+                Toast.makeText(this, "Please wait...", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+}
