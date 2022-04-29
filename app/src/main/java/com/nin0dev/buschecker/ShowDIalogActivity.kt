9b5df45d2774b795971f87f0ba54cfb70eb291bb
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-04-29 17:44:27 -0400

    Replaced normal dialog by bottom sheet

--- app/src/main/java/com/nin0dev/buschecker/ShowDIalogActivity.kt
+++ app/src/main/java/com/nin0dev/buschecker/ShowDIalogActivity.kt
@@ -2,24 +2,21 @@ package com.nin0dev.buschecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ShowDIalogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         DynamicColors.applyIfAvailable(this)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_dialog)
-        MaterialAlertDialogBuilder(this)
-            .setTitle(intent.getStringExtra("title"))
-            .setMessage(intent.getStringExtra("text"))
-            .setPositiveButton("OK") { dialog, which ->
-                finish()
-            }
-            .setOnCancelListener {
-                finish()
-            }
-            .show()
-
+        findViewById<TextView>(R.id.dialogTitle).text = intent.getStringExtra("title")
+        findViewById<TextView>(R.id.dialogText).text = intent.getStringExtra("text")
+        findViewById<Button>(R.id.okButton2).setOnClickListener {
+            finish()
+        }
     }
 }
