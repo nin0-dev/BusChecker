cd0dfdae55116eda81362a81be43ef880567029a
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-05-02 14:01:37 -0400

    Removed navbar

--- app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
+++ app/src/main/java/com/nin0dev/buschecker/MainActivity.kt
@@ -3,12 +3,11 @@ package com.nin0dev.buschecker
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsManager
-import android.view.View.GONE
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
@@ -28,8 +27,12 @@ class MainActivity : AppCompatActivity() {
             val i = Intent(this, SetupActivity::class.java)
             startActivity(i)
         }
-        navBar()
         buttons()
+        cards()
+    }
+    fun cards()
+    {
+
     }
     fun buttons()
     {
@@ -54,31 +57,5 @@ class MainActivity : AppCompatActivity() {
             recreate()
         }
     }
-    fun navBar()
-    {
-        val navbar = findViewById<NavigationBarView>(R.id.bottom_navigation)
-        navbar.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.home_menu -> {
-                    navbar.menu.getItem(0).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_baseline_home_24)
-                    navbar.menu.getItem(1).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_directions_bus_24)
-                    navbar.menu.getItem(2).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_settings_24)
-                    true
-                }
-                R.id.bus_stops_menu -> {
-                    navbar.menu.getItem(0).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_home_24)
-                    navbar.menu.getItem(1).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_baseline_directions_bus_24)
-                    navbar.menu.getItem(2).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_settings_24)
-                    true
-                }
-                R.id.settings_menu -> {
-                    navbar.menu.getItem(0).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_home_24)
-                    navbar.menu.getItem(1).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_outline_directions_bus_24)
-                    navbar.menu.getItem(2).icon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_baseline_settings_24)
-                    true
-                }
-                else -> false
-            }
-        }
-    }
+
 }
