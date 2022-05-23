d684c179972e24b8ae6cc328d4e4456deb0fbca8
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-05-23 19:21:02 -0400

    Switched to OTA version (1.0)

--- public/fr/index.js
+++ public/fr/index.js
@@ -1,6 +1,6 @@
 function download1_0() {
     const el = document.createElement('iframe');
-    el.setAttribute('src', 'https://appcenter-filemanagement-distrib3ede6f06e.azureedge.net/8b19157a-3eba-44a1-a3fe-255afaa16acf/BusChecker.apk?sv=2019-02-02&sr=c&sig=6ITHMnyQsHjWCIvLwm%2FuK4%2FOwl3Nvpm8hOgNqDbGmzU%3D&se=2022-05-23T22%3A44%3A09Z&sp=r');
+    el.setAttribute('src', 'https://appcenter-filemanagement-distrib1ede6f06e.azureedge.net/89408ef6-7a68-48cf-9d43-51c89c13161d/BusChecker-1.0.apk?sv=2019-02-02&sr=c&sig=nETSRkB0y%2BVWBSBmHMrG%2FHqQQQe3fm07ZuwPzjmEhT8%3D&se=2022-05-24T23%3A20%3A34Z&sp=r');
     el.style.display = "none";
     document.getElementById('body').appendChild(el);
     setTimeout(function () {
