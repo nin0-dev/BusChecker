e813bf0401c6142ee9607d0af2e30eb1125a7a89
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-05-24 07:28:20 -0400

    Download now happens on instructions page

--- public/fr/index.js
+++ public/fr/index.js
@@ -1,9 +1,3 @@
 function download1_0() {
-    const el = document.createElement('iframe');
-    el.setAttribute('src', 'https://appcenter-filemanagement-distrib5ede6f06e.azureedge.net/cf869e6a-a3f7-4947-8f79-f9a6dc3d5a88/app-release.apk?sv=2019-02-02&sr=c&sig=3uigvkYydIkchG3x%2Bt6t7S%2BPylaSV0Tgq5f0BtgZHGQ%3D&se=2022-05-24T23%3A36%3A21Z&sp=r');
-    el.style.display = "none";
-    document.getElementById('body').appendChild(el);
-    setTimeout(function () {
-        window.location.href = "dlinstructions.html"
-    }, 200);
+    window.location.href = "dlinstructions.html?appcenter_url=https%3A%2F%2Fappcenter-filemanagement-distrib3ede6f06e.azureedge.net%2F88ce0304-d408-42dc-aab9-893fe50e2ce6%2FBusChecker-1.0.apk%3Fsv%3D2019-02-02%26sr%3Dc%26sig%3DnwDfnJQcxan8hk7T6J8wjS%252BBJ8k9GpTanc7vpxqT2Ro%253D%26se%3D2022-05-25T11%253A13%253A59Z%26sp%3Dr"
 }
