092d236ebf891da4410d94b54253925c8770977f
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-05-28 06:42:15 -0400

    Switched to GitHub Releases

--- public/fr/index.js
+++ public/fr/index.js
@@ -1,3 +1,3 @@
 function download1_0() {
-    window.location.href = "dlinstructions.html?appcenter_url=https%3A%2F%2Fappcenter-filemanagement-distrib3ede6f06e.azureedge.net%2F88ce0304-d408-42dc-aab9-893fe50e2ce6%2FBusChecker-1.0.apk%3Fsv%3D2019-02-02%26sr%3Dc%26sig%3DnwDfnJQcxan8hk7T6J8wjS%252BBJ8k9GpTanc7vpxqT2Ro%253D%26se%3D2022-05-25T11%253A13%253A59Z%26sp%3Dr"
+    window.location.href = "dlinstructions.html?https%3A%2F%2Fgithub.com%2Fnin0-dev%2FBusChecker%2Freleases%2Fdownload%2F1.0%2FBusChecker-1.0.apk"
 }
